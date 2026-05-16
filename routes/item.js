14e9a15894f6cf12b4789cbb602d3ee6a40f0cfc
rommel dalisay <rommelmdalisay@gmail.com>
2026-05-16 11:54:54 +0800

    item crud

--- routes/item.js
+++ routes/item.js
@@ -2,10 +2,16 @@ const express = require('express');
 const router = express.Router();
 const upload = require('../utils/multer')
 
-const { getAllItems, getSingleItem, createItem } = require('../controllers/item')
+const { getAllItems,
+    getSingleItem,
+    createItem,
+    updateItem,
+    deleteItem } = require('../controllers/item')
 
 router.get('/items', getAllItems)
 router.get('/items/:id', getSingleItem)
 router.post('/items', upload.single('image'), createItem)
+router.put('/items/:id', upload.single('image'), updateItem)
+router.delete('/items/:id', deleteItem)
 
 module.exports = router;
