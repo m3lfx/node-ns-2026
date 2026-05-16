0179209f5e53df4015e4ae02cf945a9152a61db6
rommel dalisay <rommelmdalisay@gmail.com>
2026-05-16 11:00:24 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+// const items = require('./routes/item');
+
+// app.get('/', (req, res) => {
+//     res.send('Hello from nodejs!')
+// })
+
+module.exports = app
--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+const app = require('./app')
 
-app.get('/', (req, res) => {
-    res.send('Hello from nodejs!');
-});
+require('dotenv').config()
 
-app.listen(port, () => {
-    console.log(`Express server listening on port ${port}`);
-});
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
